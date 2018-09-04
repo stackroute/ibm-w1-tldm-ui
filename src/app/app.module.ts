ec9e5c580d05a7740d778e37a5fe836f44cd2938
Rajashree <rajitheprincess@gmail.com>
2018-09-04 20:46:52 +0530

    integrated message area ,header,text area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TextareaComponent } from './textarea/textarea.component';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
 
-import {  MatCheckboxModule,   MatTooltipModule } from '@angular/material';
+import { MatCheckboxModule, MatTooltipModule } from '@angular/material';
 import { MainNavComponent } from './main-nav/main-nav.component';
-import { Card1Component } from './card1/card1.component';
-import { Card2Component } from './card2/card2.component';
+
 import { IntegrationComponent } from './integration/integration.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-     Card1Component,
-     Card2Component,
+
     TextareaComponent,
     MainNavComponent,
     IntegrationComponent,
+   
+   
   ],
   imports: [
     BrowserModule,
@@ -47,6 +48,7 @@ import { IntegrationComponent } from './integration/integration.component';
     MatTooltipModule,
     MatCardModule,
   ],
+
   
   providers: [],
   bootstrap: [AppComponent]
