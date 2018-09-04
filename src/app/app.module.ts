9e8d91ff139e60910101842c2480f2ec54d442f4
Gaya397 <gaya397@gmail.com>
2018-09-04 15:16:35 +0530

    message area added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { Card1Component } from './card1/card1.component';
+import { Card2Component } from './card2/card2.component';
+import { LayoutModule } from '@angular/cdk/layout';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Card1Component,
+    Card2Component
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
+  entryComponents:[],
   providers: [],
   bootstrap: [AppComponent]
 })
