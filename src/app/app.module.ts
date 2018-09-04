35336dbdcd93fd50dec1c1bbf6a906a98f42fa35
Gaya397 <gaya397@gmail.com>
2018-09-04 16:18:34 +0530

    changes in text and message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { MatCardModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Card1Component } from './card1/card1.component';
-import { Card2Component } from './card2/card2.component';
-import { LayoutModule } from '@angular/cdk/layout';
-@NgModule({
-  declarations: [
-    AppComponent,
-    Card1Component,
-    Card2Component
-=======
 import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,36 +7,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TextareaComponent } from './textarea/textarea.component';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
 
 import {  MatCheckboxModule,   MatTooltipModule } from '@angular/material';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { Card1Component } from './card1/card1.component';
+import { Card2Component } from './card2/card2.component';
+import { IntegrationComponent } from './integration/integration.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
+     Card1Component,
+     Card2Component,
     TextareaComponent,
-    MainNavComponent
->>>>>>> c9f9623764b93facdf1bea470b1a95821f1c8ffa
+    MainNavComponent,
+    IntegrationComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
-    MatCardModule,
-=======
-
->>>>>>> c9f9623764b93facdf1bea470b1a95821f1c8ffa
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-<<<<<<< HEAD
-=======
     NoopAnimationsModule,
     MatInputModule,
 
@@ -61,10 +44,10 @@ import { MainNavComponent } from './main-nav/main-nav.component';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatTooltipModule
->>>>>>> c9f9623764b93facdf1bea470b1a95821f1c8ffa
+    MatTooltipModule,
+    MatCardModule,
   ],
-  entryComponents:[],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
