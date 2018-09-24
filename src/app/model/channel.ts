4ff7bdc42472cd1d3fa7ced4b1a198703b8397d9
manikg11 <manikg11@in.ibm.com>
2018-09-24 09:38:32 +0530

    fixed channel.ts

--- src/app/model/channel.ts
+++ src/app/model/channel.ts
@@ -4,16 +4,16 @@ import {User} from './user';
 export class Channel {
     channelId: string;
     channelName: string;
-    createdBy: string;
+    channelCreatedBy: string;
     channelDescription: string;
     channelCreatedDate: string;
     community: Community;
     channelUsers: User[];
 
-    constructor(channelName: string, channelDescription: string, channelUsers: User[], createdBy: string) {
+    constructor(channelName: string, channelDescription: string, channelUsers: User[], channelCreatedBy: string) {
         this.channelName = channelName;
         this.channelDescription = channelDescription;
         this.channelUsers = channelUsers;
-        this.createdBy = createdBy;
+        this.channelCreatedBy = channelCreatedBy;
     }
 }
