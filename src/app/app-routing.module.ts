2e8c8ec5c8eabad9f68cd9bdc4a5c713a3da2c8f
Kartikey <kmishr11@in.ibm.com>
2018-09-25 20:22:24 +0530

    implemented search functionality v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import {ChannelPageComponent} from './channel-page/channel-page.component';
 import {AuthGuardService as AuthGuard} from './service/auth-guard.service';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
--- src/app/search-dialog/search-dialog.component.ts
+++ src/app/search-dialog/search-dialog.component.ts
@@ -1,6 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {SearchService} from '../service/search.service';
 import {User} from '../model/user';
+import {TokenStorage} from '../service/token-storage.service';
+import {MessageService} from '../service/message.service';
+import {ChannelService} from '../service/channel.service';
+import {UserService} from '../service/user.service';
+import {Message} from '../model/message';
+import {Router} from '@angular/router';
+import {MatDialogRef} from '@angular/material';
 
 @Component({
     selector: 'app-search-dialog',
@@ -10,8 +17,16 @@ import {User} from '../model/user';
 export class SearchDialogComponent implements OnInit {
 
     results: User[];
+    user: User;
+    messages: Message[];
 
-    constructor(private searchService: SearchService) {
+    constructor(private searchService: SearchService,
+                private tokenStorage: TokenStorage,
+                private messageService: MessageService,
+                private channelService: ChannelService,
+                private userService: UserService,
+                private router: Router,
+                public dialogRef: MatDialogRef<SearchDialogComponent>) {
     }
 
     ngOnInit() {
@@ -21,6 +36,10 @@ export class SearchDialogComponent implements OnInit {
         this.searchService.searchTerm(searchTerm).subscribe(results => {
             console.log(this.results = results);
         });
+    }
+
+    setReceiver() {
+        console.log('hello');
     }
 
 }
